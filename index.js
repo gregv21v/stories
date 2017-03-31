5934136959d7ba91ec2f0eb5f1a485b10a6a122d
gregv21v <gregv21v@gmail.com>
2017-03-30 20:55:12 -0400

    Files Moved & Express

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+var app = express();
+
+app.use(express.static('public'));
+
+app.get('/', function(req, res) {
+  res.sendFile(
+    path.join(__dirname, "public", "index.html")
+  );
+})
+
+
+app.listen(3000, function() {
+  console.log("Example app on port 3000!");
+})
